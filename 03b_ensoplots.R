a9a1edfedf968e89b894dfe0bbda1ad7f78a4010
Brendan Barrett <bbarrett@ab.mpg.de>
2023-06-02 13:34:53 +0200

    axis fixed

--- 03b_ensoplots.R
+++ 03b_ensoplots.R
@@ -8,8 +8,8 @@ for(i in 1:24){
   points(list_area_2$mei[list_area_2$year_index_mei==i] , rep(0,12), col="red")
 }
 
-plot(list_area_2$mean_annual_mei,list_area_2$hr_area_mean , xlab="home range area (ha)" ,
-     ylab="monthly ENSO index" , col=group_pal[list_area_2$group_index], xlim=c(-3,3))
+plot(list_area_2$mean_annual_mei,list_area_2$hr_area_mean , ylab="home range area (ha)" ,
+     xlab="monthly ENSO index" , col=group_pal[list_area_2$group_index], xlim=c(-3,3))
 
 # for (obs in 1:nrow(d_hr_gs_3)){
 #   points( post$mei_dist[1:100, list_area_2$year_index[obs] ] , 
@@ -26,7 +26,4 @@ for(i in 1:nrow(d_hr_gs_3)){
          rep(list_area_2$hr_area_mean[i] , 12 ) ,
          col=col.alpha(group_pal[list_area_2$group_index[i] ]) )
 }
-
-
-
 
